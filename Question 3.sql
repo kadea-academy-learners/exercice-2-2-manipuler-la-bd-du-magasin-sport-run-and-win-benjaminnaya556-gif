e412cdb6cb48123f93797279c2c285cefcb3dcc9
benjaminnaya556-gif <benjaminnaya556@gmail.com>
2026-04-23 19:03:23 +0200

    Delete Question 3.sql

--- Question 3.sql
+++
@@ -1,8 +0,0 @@
-/***3.Le filtrage des "Villes Prioritaires"
-"On lance une campagne d'affichage à Bukavu et Matadi. Donne-moi le nom, 
-le prénom et l'email de tous nos clients qui habitent soit à 'Bukavu', soit à 'Matadi'."***/
-
-SELECT nom, prenom,ville,  email FROM clients WHERE ville = 'Bukavu' OR ville ='Matadi'
-
-
-SELECT nom, prenom,ville , email FROM clients WHERE ville IN ('Bukavu','Matadi')
