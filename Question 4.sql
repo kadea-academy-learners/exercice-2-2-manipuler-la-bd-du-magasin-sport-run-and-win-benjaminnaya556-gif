12e77e4812de53dfefcccbbe9788116b412ba9c8
benjaminnaya556-gif <benjaminnaya556@gmail.com>
2026-04-23 19:03:37 +0200

    Delete Question 4.sql

--- Question 4.sql
+++
@@ -1,8 +0,0 @@
-/***4. La traque des "Anciens Articles" (Le juste prix)
-"Je cherche les articles de sport de combat (catégorie 'Combat') 
-dont le prix se situe entre 20€ et 50€. On va leur appliquer une remise immédiate."***/
-
-SELECT * FROM produits WHERE categorie = 'Combat'AND prix BETWEEN 20 AND 50
-
-
-SELECT * FROM produits WHERE categorie = 'Combat' AND prix >=20 AND prix <=50
