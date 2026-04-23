6fb7bc50d1b3b60d2872bbb554d5c079e18d8024
benjaminnaya556-gif <benjaminnaya556@gmail.com>
2026-04-23 19:03:04 +0200

    Delete Question 5.sql

--- Question 5.sql
+++
@@ -1,7 +0,0 @@
-/***5. Le casse-tête des "Inactifs de Lubumbashi"
-"Dernière chose : je veux relancer les clients de Lubumbashi (ville = 'Lubumbashi')
- qui se sont inscrits avant l'année 2025. Affiche leur nom, leur ville et leur date d'inscription."***/
-
-SELECT nom , ville , date_inscription
-FROM clients WHERE ville = 'Lubumbashi'
-AND STRFTIME('%Y', date_inscription) < '2025'
